e308538c8196a05e69150309b09e6137f3c39d39
Almann Goo <almann.goo@gmail.com>
2015-12-24 18:59:52 -0800

    Adds horizontal tile support.

--- WindowMatrix.ino
+++ WindowMatrix.ino
@@ -2,40 +2,50 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
-static const uint8_t  kMatrixPin    = 4;
-static const int      kMatrixRows   = 11;
-static const int      kMatrixCols   = 12;
-static const int      kMatrixPixels = kMatrixRows * kMatrixCols;
+namespace {
 
-static Adafruit_NeoMatrix matrix(
-  kMatrixCols, kMatrixRows, kMatrixPin,
-  NEO_MATRIX_TOP | NEO_MATRIX_LEFT | NEO_MATRIX_ZIGZAG,
-  NEO_RGB | NEO_KHZ400
-);
+  const uint8_t  kMatrixPin    = 4;
+  const int      kMatrixRows   = 11;
+  const int      kMatrixCols   = 12;
+  const int      kMatrixPixels = kMatrixRows * kMatrixCols;
+  const int      kMatrixTiles  = 4;
 
-// simple color definition
-struct RGB {
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
-};
+  static Adafruit_NeoMatrix matrix(
+    kMatrixCols, kMatrixRows, kMatrixTiles, 1, kMatrixPin,
+    NEO_MATRIX_TOP | NEO_MATRIX_LEFT | NEO_MATRIX_ZIGZAG,
+    NEO_RGB | NEO_KHZ400
+  );
 
-// pull in the code generated images
-// generates:
-//
-//  static const RGB    kImages[][kMatrixPixels] = {...};
-//  static const size_t kImageCount = ...;
-//
-#include "Images.h"
+  // simple color definition
+  struct RGB {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+  };
 
-// draws an RGB array assuming row-ordered, progressive flattened array
-void draw_rgb_array(RGB const *pixels) {
-  for (int x = 0; x < kMatrixCols; x++) {
-    for (int y = 0; y < kMatrixRows ; y++) {
-      uint16_t color = matrix.Color(pixels->r, pixels->g, pixels->b);
-      matrix.drawPixel(x,y, color);
-      pixels++;
+  // pull in the code generated images
+  // generates:
+  //
+  //  static const RGB    kImages[][kMatrixPixels] = {...};
+  //  static const size_t kImageCount = ...;
+  //
+  #include "Images.h"
+
+  // draws an RGB array assuming row-ordered, progressive flattened array
+  void draw_rgb_array(int const tile, RGB const *pixels) {
+    int xOffset = tile * kMatrixCols;
+    for (int x = 0; x < kMatrixCols; x++) {
+      for (int y = 0; y < kMatrixRows ; y++) {
+        uint16_t color = matrix.Color(pixels->r, pixels->g, pixels->b);
+        matrix.drawPixel(x + xOffset, y, color);
+        pixels++;
+      }
     }
+  }
+
+  inline RGB const *image_at(size_t index) {
+    index = index % kImageCount;
+    return kImages[index];
   }
 }
 
@@ -46,8 +56,12 @@ void setup() {
 
 void loop() {
   for (int i = 0; i < kImageCount; i++) {
-    draw_rgb_array(kImages[i]);
+    for (int j = 0; j < kMatrixTiles; j++) {
+      draw_rgb_array(j, image_at(i + j));  
+    }
     matrix.show();
-    delay(5000);
+    delay(3000);
+
+    // transition to the next image
   }
 }
