beea4353276bcd4363d4d90f8fceaec49a04eb20
Almann Goo <almann.goo@gmail.com>
2016-10-31 01:09:44 -0700

    Very rough in-progress definitions for HolidayCode.

--- HolidayCode.h
+++ HolidayCode.h
@@ -19,14 +19,30 @@
 // Also part of this module is a simple command interface for storing winow matrix 'programs'
 // and configuring the micro-controller that can be tunneled through serial or as a simeple
 // RPC protocol over the WiFi interface.  The format is a 'dfmt' message in the p-code form above,
-// but has a synchronous response element that is a 'dfmt' message in the p-code form.
+// but has a synchronous response element that is a 'dfmt' message in the p-code form.  Generally
+// the U16_INT in the response is an error code, where 0x00 is 'OK', followed by any other information
+// in the list.  In the request and response forms, the list is always required.
 //
 
 #include "DataFormat.h"
 
 namespace hcode {
+  enum class ControlOp : uint16_t {
+    PROGRAM = 0x00,
+    INFO,
+    WIFI,
+    CLEAR
+  };
+
+  enum class AnimationOp : uint16_t {
+    SLEEP = 0x00,
+    MARQUEE,
+    ANIMATE
+  };
   
-  
+  class ControlProcessor {
+
+  };
 }
 
 #endif
